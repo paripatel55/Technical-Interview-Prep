2d67a61d0d7f9a0a8758a99d9282e6637e0d3d27
Pari Patel <paripatel1515@gmail.com>
2025-11-20 16:22:54 -0500

    chore: initial commit

---
+++ Codesignal/Question 2s/arrayTriplets.py
@@ -0,0 +1,10 @@
+'''
+Array Triplets with Pythagorean Property
+Given an integer array of length n, return a result array of length n-2, where result[i] = 1 if:
+
+array[i]^2 + array[i+1]^2 = array[i+2]^2,
+array[i+1]^2 + array[i+2]^2 = array[i]^2, and
+array[i]^2 + array[i+2]^2 = array[i+1]^2.
+Otherwise, result[i] = 0.
+'''
+
